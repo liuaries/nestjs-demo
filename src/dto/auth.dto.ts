ab5ee57acd6e193d051591e806a0eb3d61c939ed
liuhui <liuhui@ihomefnt.com>
2020-01-07 16:21:04 +0800

    plugins

--- src/dto/auth.dto.ts
+++ src/dto/auth.dto.ts
@@ -5,10 +5,10 @@ export class LoginDto {
   @MinLength(4, { message: 'name must not be to short' })
   @MaxLength(12, { message: 'name must not be to long' })
   @IsString({ message: 'name must be string' })
-  @ApiProperty()
+  @ApiProperty({ example: 'liuhui', description: '姓名' })
   readonly name: string;
 
   @IsString({ message: 'password must be string' })
-  @ApiProperty()
+  @ApiProperty({ example: '$2b$10$InzTTbwNKuB4vQdpSUoMQ.z5TGJLd/em.FaTTxNnkyVMZkMoQRa82', description: '密码' })
   readonly password: string;
 }
--- src/dto/create-cat.dto.ts
+++ src/dto/create-cat.dto.ts
@@ -5,14 +5,14 @@ export class CreateCatDto {
   @MinLength(2, { message: 'name must not be to short' })
   @MaxLength(8, { message: 'name must not be to long' })
   @IsString({ message: 'name must be string' })
-  @ApiProperty()
+  @ApiProperty({ example: '小黑', description: '姓名' })
   readonly name: string;
 
   @IsInt({ message: 'age must be number' })
-  @ApiProperty()
+  @ApiProperty({ example: 2, description: '年龄' })
   readonly age: number;
 
   @IsString({ message: 'breed must be string' })
-  @ApiProperty()
+  @ApiProperty({ example: '泰迪', description: '品种' })
   readonly breed: string;
 }
