b349e61313a1729b9b7f0de1c77dd50ccc5137ae
liuhui <liuhui@ihomefnt.com>
2020-01-16 18:57:04 +0800

    细节调整

--- src/controller/upload.controller.ts
+++ src/controller/upload.controller.ts
@@ -1,7 +1,7 @@
 import { ApiException } from './../infrastructure/exception/api.exception';
 import { Controller, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
-import { ApiTags, ApiBody, ApiConsumes } from '@nestjs/swagger';
+import { ApiTags, ApiBody, ApiConsumes, ApiOperation } from '@nestjs/swagger';
 import { createWriteStream } from 'fs';
 import { join } from 'path';
 
@@ -10,6 +10,7 @@ import { FileUploadReq, FilesUploadReq } from './../dto/request/file.upload.req'
 @ApiTags('UploadController')
 @Controller('upload')
 export class UploadController {
+    @ApiOperation({summary: '单文件上传'})
     @ApiConsumes('multipart/form-data')
     @ApiBody({
         description: '文件上传',
@@ -18,12 +19,14 @@ export class UploadController {
     @Post('file')
     @UseInterceptors(FileInterceptor('file'))
     uploadFile(@UploadedFile() file): string {
-        const writeImage = createWriteStream(join(__dirname, '../../upload/files', `${Date.now()}-${file.originalname}`))
+        const path = join(__dirname, '../../upload/files', `${Date.now()}-${file.originalname}`)
+        const writeImage = createWriteStream(path)
         writeImage.write(file.buffer)
 
-        return '上传成功';
+        return path;
     }
 
+    @ApiOperation({summary: '多文件上传'})
     @ApiConsumes('multipart/form-data')
     @ApiBody({
         description: '文件上传',
@@ -31,14 +34,17 @@ export class UploadController {
     })
     @Post('files')
     @UseInterceptors(FilesInterceptor('files'))
-    uploadFiles(@UploadedFiles() files: any[]): string {
+    uploadFiles(@UploadedFiles() files: any[]): string[] {
         if(files.length === 0){
             throw new ApiException('参数有误')
         }
+        let urls = [];
         for (const file of files) {
-            const writeImage = createWriteStream(join(__dirname, '../../upload/files', `${Date.now()}-${file.originalname}`))
+            const path = join(__dirname, '../../upload/files', `${Date.now()}-${file.originalname}`)
+            const writeImage = createWriteStream(path)
             writeImage.write(file.buffer)
+            urls.push(path)
         }
-        return '上传成功';
+        return urls;
     }
 }
