27929de4fd02aeb19c4b0d8bcb14fecf6d6d7909
yangtao <yangtao@ihomefnt.com>
2020-02-28 17:41:41 +0800

    obj2glb

--- src/service/transform.service.ts
+++ src/service/transform.service.ts
@@ -1,20 +1,29 @@
 import { Injectable } from '@nestjs/common';
-import * as process from 'child_process'
+import * as process from 'child_process';
 import { join } from 'path';
+import * as obj2gltf from 'obj2gltf';
+import * as fs from 'fs';
+
+const path = join(__dirname, '../../lib')
 
 @Injectable()
 export class TransformService {
   async mx2obj() {
-    let success = true
-    const path = join(__dirname, '../../lib')
-    process.exec(`${path}/MxToObj.exe -f ${path}/test.mx`, (error, stdout, stderr)=> {
-      if(error){
-        success = false
+    let success:boolean = false
+    process.exec(`${path}/MxToObj.exe -f ${path}/test.mx`, async(error, stdout, stderr)  => {
+      if(!error){
+        success = await this.obj2glb();
       }
-      console.log(error)
-      console.log(stdout)
-      console.log(stderr)
+      return success
     })
-    return success
+  }
+
+  async obj2glb(): Promise<boolean> {
+    const glb = await obj2gltf(`${path}/test.mx.obj`,{binary: true});
+    await fs.writeFileSync(`${path}/test.glb`,glb);
+
+    return true
   }
 }
+
+
